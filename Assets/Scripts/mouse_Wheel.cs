06c4d9d2a04277d3aec0e84b82289e8b8e9d2a76
Valle1806 <vallecilla.cristian@correounivalle.edu.co>
2020-12-11 23:32:02 -0500

    feat: mouse script

---
+++ Assets/Scripts/mouse_Wheel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouse_Wheel : MonoBehaviour
+{
+    public float ZoomAmount = 0.0f; //With Positive and negative values
+    public float MaxToClamp = 10.0f;
+    public float ROTSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+ 
+    // Update is called once per frame
+    private void Update() {
+     ZoomAmount += Input.GetAxis("Mouse ScrollWheel");
+     ZoomAmount = Mathf.Clamp(ZoomAmount, -MaxToClamp, MaxToClamp);
+     var translate = Mathf.Min(Mathf.Abs(Input.GetAxis("Mouse ScrollWheel")), MaxToClamp - Mathf.Abs(ZoomAmount));
+     gameObject.transform.Translate(0,0,translate * ROTSpeed * Mathf.Sign(Input.GetAxis("Mouse ScrollWheel")));
+    }
+}
---
+++ Assets/Scripts/mouse_move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouse_move : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform cam;
+ 
+    private void turn_player()
+    {
+        player.eulerAngles = new Vector3(0.0f, cam.eulerAngles.y, 0.0f);
+        cam.transform.position = player.transform.position;
+
+        player.eulerAngles = new Vector3(cam.eulerAngles.x, 0.0f, 0.0f);
+        cam.transform.position = player.transform.position;
+    }
+ 
+    private void Update()
+    {
+        turn_player();
+    }
+}
