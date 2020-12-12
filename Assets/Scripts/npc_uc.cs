876583efadb3d432fae9470d5afae7deef989e07
Christian Taborda <christian.taborda@correounivalle.edu.co>
2020-12-11 22:57:33 -0500

    NPC Character
    - NPC Character Scripts

---
+++ Assets/Scripts/npc_uc.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npc_uc : MonoBehaviour
+{
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player"){
+            anim.Play("WIN00", -1, 0f);
+        }
+        //anim.Play("WIN00", -1, 0f);
+    }
+}
